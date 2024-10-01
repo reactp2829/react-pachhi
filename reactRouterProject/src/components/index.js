8676ff3aa33e7360b7fc6cb1d6125632cad5dd86
reactp2829 <reactpachhi@gmail.com>
2024-10-01 16:00:55 +0530

    react router files

---
+++ reactRouterProject/src/components/index.js
@@ -0,0 +1,6 @@
+import Home from "./Home/Home";
+import About from "./About/About";
+import Contactus from "./Contactus/Contactus";
+import User from './User/User.jsx'
+import Github from './Github/Github.jsx'
+export{Home,About,Contactus,User,Github}
